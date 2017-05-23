e5f58248df0f80fa0b3f9bc7f35c817a53ce8570
Mihail Fernandes <fernandesbg88@gmail.com>
2017-05-23 17:25:19 +0300

    C# Intro and Basic Syntax
    Programming Fundamentals - Extended - май 2017

---
+++ 3 C# Intro and Basic Syntax/01 BasicSyntaxDemo/Problem 02/Problem 02 Sum numbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOne = int.Parse(Console.ReadLine());
+            var numberTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{numberOne} + {numberTwo} = {numberOne+numberTwo}");
+        }
+    }
+}
---
+++ 3 C# Intro and Basic Syntax/01 BasicSyntaxDemo/Problem 03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var iD = int.Parse(Console.ReadLine());
+            var monthlySalary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {iD:D8}");
+            Console.WriteLine($"Salary: {monthlySalary:f2}");
+
+        }
+    }
+}
